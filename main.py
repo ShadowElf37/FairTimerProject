94dadfbdda567a10eea0419f967f27a73a73dc72
alqaadi <yovelkeycohen@gmail.com>
2019-01-15 17:32:59 -0600

    fleshed out timing e system

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from time import time, sleep
+import datetime
 from threading import Thread
 from tkinter import *
+from sys import exit
 
 # Read Config
 queue = open('config', 'r').read().split('\n')
 log = open('log', 'a')
+
+
+def get_time(sec=True):
+    return datetime.datetime.now().strftime('%H:%M'+(':%S' if sec else '')+'%p').lower()
+def get_date():
+    return datetime.datetime.now().strftime('%d/%m/%Y')
+def timestamp():
+    return datetime.datetime.now()
 
 def parse_hms(st):
     h = st.find('h')
@@ -20,6 +30,12 @@ def parse_hms(st):
 
     return hours*3600 + minutes*60 + seconds
 
+
+class TimedCommand:
+    def __init__(self, time, cmdtext):
+        self.time = time
+        self.text = cmdtext
+
 # Countdown timer object
 class Countdown:
     def __init__(self, t):
@@ -27,6 +43,7 @@ class Countdown:
         self.returnval = 0
         self.t = t
         self.start = -1
+        self.timed_cmds = []
 
     def tick(self):
         if self.returnval:
@@ -37,7 +54,15 @@ class Countdown:
             self.returned = True
             self.returnval = 1
             return True
+
+        for tc in self.timed_cmds:
+            if self.t - (time() - self.start) <= tc.time:
+                return tc.text
+
         return False
+
+    def timed_command(self, time_left, text):
+        self.timed_cmds.append(TimedCommand(parse_hms(time_left), text))
 
 
 # Countup timer object
@@ -59,3 +84,67 @@ class Countup:
 
     def stop(self):
         self.returned = True
+
+
+root = Tk()
+current_timer = None
+prompt = 'Ready.'
+i = 0
+
+while True:
+    sleep(0.01)
+    try:
+        root.update()
+        root.update_idletasks()
+
+        if current_timer is None:
+            cmd = queue[i]
+            c = cmd.split(' ')
+
+            if c[0] == 'await_time':
+                if get_time(sec=False) != c[1]:
+                    continue
+            elif c[0] == 'prompt':
+                prompt = cmd[cmd.find('"'):cmd.find('"', cmd.find('"')+1)]
+
+            elif c[0] == 'countdown': # MUST SET UP
+                current_timer = Countdown(parse_hms(c[1]))
+                extras = cmd.split('-')[1:]
+                for arg in extras:
+                    time = arg[:arg.find('(')]
+                    s = arg[arg.find('('):arg.find(')', arg.find(')')+1)]
+                    
+                    current_timer.timed_command(time, s)
+                holding = True
+
+            elif c[0] == 'countup':
+                current_timer = Countup()
+                holding = True
+
+            elif c[0] == 'play':
+                f = cmd[cmd.find('"'):cmd.find('"', cmd.find('"')+1)]
+
+            i += 1
+
+        elif current_timer:
+            result = current_timer.tick()
+            if result is False:
+                continue
+            elif result is True:
+                rv = current_timer.returnval
+                if rv is not 1:
+                    log.write(timestamp()+' - '+prompt+' - %d' % rv)
+                current_timer = None
+            else:
+                c = result.split(' ')
+                if c[0] == 'prompt':
+                    prompt = cmd[cmd.find('"'):cmd.find('"', cmd.find('"')+1)]
+                
+                elif c[0] == 'play':
+                    f = cmd[cmd.find('"'):cmd.find('"', cmd.find('"')+1)]
+
+    except KeyboardInterrupt:
+        break
+
+root.destroy()
+exit()
