d9df66bc2ad1da1e404720b1d6a504014d53c59c
ShadowElf37 <ykey-cohen@emeryweiner.org>
2019-01-15 07:22:26 -0600

    made new config layout; should be near-final modified timer classes and added XhXmXs parser func

--- main.py
+++ main.py
@@ -4,22 +4,40 @@ from tkinter import *
 
 # Read Config
 queue = open('config', 'r').read().split('\n')
+log = open('log', 'a')
+
+def parse_hms(st):
+    h = st.find('h')
+    m = st.find('m')
+    s = st.find('s')
+    hours = minutes = seconds = 0
+    if h != -1:
+        hours = int(st[:h])
+    if m != -1:
+        minutes = int(st[h+1:m])
+    if s != -1:
+        seconds = int(st[max(m,h)+1:s])
+
+    return hours*3600 + minutes*60 + seconds
 
 # Countdown timer object
 class Countdown:
-    def __init__(self):
+    def __init__(self, t):
         self.returned = False
         self.returnval = 0
+        self.t = t
+        self.start = -1
 
-    def begin(self, t):
-        self.returned = False
-        start = time()
-        while time() - start < t:
-            sleep(0.001)
-        self.returned = True
-
-    def gen_thread(self, t):
-        return Thread(target=self.begin, args=t)
+    def tick(self):
+        if self.returnval:
+            return
+        if self.start == -1:
+            self.start = time()
+        if time() - self.start >= self.t:
+            self.returned = True
+            self.returnval = 1
+            return True
+        return False
 
 
 # Countup timer object
@@ -27,17 +45,17 @@ class Countup:
     def __init__(self):
         self.returned = False
         self.returnval = 0
+        self.start = -1
 
-    def begin(self):
-        self.returned = False
-        self.returnval = 0
-        start = time()
-        while not self.returned:
-            sleep(0.001)
-        self.returnval = time() - start
+    def tick(self):
+        if self.returnval:
+            return
+        if self.start == -1:
+            self.start = time()
+        if self.returned:
+            self.returnval = time() - self.start
+            return True
+        return False
 
     def stop(self):
         self.returned = True
-
-    def gen_thread(self):
-        return Thread(target=self.begin)
