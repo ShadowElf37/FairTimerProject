702c5e1ace530d162cc0bf2e0adf01c685de678b
ShadowElf37 <ykey-cohen@emeryweiner.org>
2019-01-16 20:11:42 -0600

    hotfix

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 import sys
+import os
 from cx_Freeze import setup, Executable
 
 # Dependencies are automatically detected, but it might need fine tuning.
 build_exe_options = {"packages": ["tkinter", "pyaudio"], "include_files": ["tcl86t.dll", "tk86t.dll", "default_config.cfg", "default_log.log"]}#, "excludes": ["tkinter"]}
+os.environ['TCL_LIBRARY'] = r'C:\Program Files\Python35-32\tcl\tcl8.6'
+os.environ['TK_LIBRARY'] = r'C:\Program Files\Python35-32\tcl\tk8.6'
+# Note that these paths are wrong :)
 
 # GUI applications require a different base on Windows (the default is for a
 # console application).
