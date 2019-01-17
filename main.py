354851675e51a735f4c40f53fd9f85e5d09957ae
ShadowElf37 <ykey-cohen@emeryweiner.org>
2019-01-16 19:36:25 -0600

    finished programming labels, added buttons, fixed some bugs, and added configurable cfg, log, and audio files - V1 is complete

--- main.py
+++ main.py
@@ -7,13 +7,14 @@ from sys import exit
 import audio
 
 # Read Config
-configfile = 'config'
+configfile = 'config.cfg'
 queue = open(configfile, 'r').read().split('\n')
-log = open('log', 'a')
+log = open('log.log', 'a')
+audiofolder = '/'
 
 
 def get_time(sec=True):
-    return str(datetime.datetime.now().strftime('%I:%M'+('.%S' if sec else '')+'%p').lower())
+    return str(datetime.datetime.now().strftime('%I:%M'+(':%S' if sec else '')+'%p').lower())
 def get_date():
     return str(datetime.datetime.now().strftime('%d/%m/%Y'))
 def timestamp():
@@ -36,8 +37,10 @@ def parse_hms(st):
 def sec_to_hms(sec):
     m = sec // 60
     h = m // 60
+    h = h % 24
+    m = m % 60
     s = sec % 60
-    return '%s:%s.%s' % (h,m,s)
+    return '%02d:%02d:%02d' % (h,m,s)
 
 
 class TimedCommand:
@@ -53,13 +56,15 @@ class Countdown:
         self.t = t
         self.start = -1
         self.timed_cmds = []
+        self.pause_time = -1
 
     def tick(self):
         if self.returnval:
             return
         if self.start == -1:
-            self.start = time()
-        if time() - self.start >= self.t:
+            self.start = round(time())
+            self.pause_time = self.start
+        if time() - self.start >= self.t and not paused:
             self.returned = True
             self.returnval = 1
             return True
@@ -72,8 +77,16 @@ class Countdown:
         return False
 
     def get_time(self):
-        return self.t - (time() - self.start)
+        global paused
+        if not paused:
+            return self.t - (time() - self.start)
+        return self.t - self.pause_time + self.start
 
+    def pause(self):
+        self.pause_time = time()
+
+    def unpause(self):
+        self.start += time() - self.pause_time
 
     def timed_command(self, time_left, text):
         self.timed_cmds.append(TimedCommand(parse_hms(time_left), text))
@@ -90,12 +103,14 @@ class DeadTimer:
 
     def tick(self):
         return False
-
     def get_time(self):
         return 0
-
     def get_time_str(self):
         return self.specialtext
+    def pause(self):
+        pass
+    def unpause(self):
+        pass
 
 # Countup timer object
 class Countup:
@@ -103,31 +118,44 @@ class Countup:
         self.returned = False
         self.returnval = 0
         self.start = -1
+        self.pause_time = -1
+        self.unpause_time = -1
 
     def tick(self):
         if self.returnval:
             return
         if self.start == -1:
             self.start = time()
+            self.pause_time = self.start
         if self.returned:
             self.returnval = self.get_time()
             return True
         return False
 
+    def pause(self):
+        self.pause_time = time()
+
+    def unpause(self):
+        self.start += time() - self.pause_time
+
     def get_time(self):
-        return time() - self.start
+        global paused
+        if not paused:
+            return time() - self.start
+        return self.pause_time - self.start
 
     def get_time_str(self):
         return sec_to_hms(round(self.get_time()))
 
     def stop(self):
         self.returned = True
-        log.write('Timer '+prompt+' - Stopped at' + str(time() - self.start)+', '+timestamp())
+        log.write('Timer with prompt "'+prompt+'" - Stopped at ' + str(round(self.get_time(), 2))+', '+timestamp()+'; line %s\n' % i)
 
 
 current_timer = DeadTimer()
-prompt = 'Ready.'
+prompt = 'No prompt set yet.'
 i = 0
+paused = False
 
 root = Tk()
 width = 1000
@@ -136,16 +164,80 @@ root.geometry('%sx%s'%(width,height))
 root.title('Mr. Fair\'s Super Special Timer')
 root.configure(background='black')
 
-time_label = Label(root, fg='white', bg='black', font=('Helvetica', 50))
+time_label = Label(root, fg='#0F0', bg='black', font=('Helvetica', 50))
 time_label.place(x=width/15, y=height/10)
 config_file_label = Label(root, fg='white', bg='black', font=('Helvetica', 26))
 config_file_label.place(x=width/15, y=height/10*3)
-prompt_label = Label(root, fg='#F31', bg='black', font=('Helvetica', 50))
+prompt_label = Label(root, fg='#F00', bg='black', font=('Helvetica', 50))
 prompt_label.place(x=width/15, y=height/10*4)
-timer_label = Label(root, fg='#0FF', bg='black', font=('Helvetica', 80))
-timer_label.place(x=width/15, y=height/10*5)
+timer_label = Label(root, fg='#02F', bg='black', font=('Helvetica', 80))
+timer_label.place(x=width/15, y=height/10*6)
 timer_type_label = Label(root, fg='white', bg='black', font=('Helvetica', 20))
-timer_type_label.place(x=width/7, y=height/10*6.2)
+timer_type_label.place(x=width/14, y=height/10*5.7)
+
+
+def load_cfg():
+    global configfile, queue, i, current_timer
+    current_timer = DeadTimer()
+    i = 0
+    configfile = filedialog.askopenfilename(initialdir="/", title="Select file",
+                                               filetypes=(("cfg files", "*.cfg"), ("all files", "*.*")))
+    #configfile = param_entry.get()
+    queue = open(configfile, 'r').read().split('\n')
+    configfile = configfile.split('/')[-1]
+    print('cfg file set')
+
+def switch_logfile():
+    global log
+    log.close()
+    f = filedialog.askopenfilename(initialdir="/", title="Select file",
+                                                 filetypes=(("log files", "*.log"), ("all files", "*.*")))
+    #f = param_entry.get()
+    log = open(f, 'a')
+    print('log file set')
+
+def find_audio():
+    global audiofolder
+    audiofolder = filedialog.askdirectory()
+    print('audio folder set')
+
+def pause():
+    global paused, current_timer
+    paused = True
+    current_timer.pause()
+
+def resume():
+    global paused, current_timer
+    paused = False
+    current_timer.unpause()
+
+def skip():
+    global i, current_timer, paused
+    paused = False
+    if isinstance(current_timer, DeadTimer): i += 1
+    current_timer = DeadTimer()
+
+def stop():
+    global current_timer, paused, i
+    paused = False
+    current_timer.stop()
+    current_timer = DeadTimer()
+
+bw = 10
+load_cfg_button = Button(root, text="Load Config", font=('Helvetica', 18), width=bw, command=load_cfg)
+load_cfg_button.place(x=width/8*6, y=height/11*3, anchor=CENTER)
+check_logs_button = Button(root, text="Set Log", font=('Helvetica', 18), width=bw, command=switch_logfile)
+check_logs_button.place(x=width/8*6, y=height/11*5, anchor=CENTER)
+find_audio_button = Button(root, text="Find Audio", font=('Helvetica', 18), width=bw, command=find_audio)
+find_audio_button.place(x=width/8*6, y=height/11*1, anchor=CENTER)
+pause_button = Button(root, text="Pause", font=('Helvetica', 18), width=bw)
+pause_button.place(x=width/8*6, y=height/11*7, anchor=CENTER)
+skip_button = Button(root, text="Skip", font=('Helvetica', 18), width=bw)
+skip_button.place(x=width/8*6, y=height/11*9, anchor=CENTER)
+
+# param_entry = Entry(root, width=30, font=('Lucida Console', 14))
+# param_entry.place(x=width/8*6, y=height/11*10, anchor=CENTER)
+# param_entry.insert(0, 'Enter file names here')
 
 while True:
     sleep(0.01)
@@ -153,7 +245,13 @@ while True:
     config_file_label.configure(text='Loaded: '+configfile)
     prompt_label.configure(text=prompt)
     timer_label.configure(text=current_timer.get_time_str())
-    timer_type_label.configure(text='Counting down.' if isinstance(current_timer, Countdown) else 'Counting up.' if isinstance(current_timer, Countup) else 'Holding.')
+    timer_type_label.configure(text=('Counting down.' if isinstance(current_timer, Countdown) else 'Counting up.' if isinstance(current_timer, Countup) else 'Holding.') + ' (Line %s)' % i)
+    skip_button.configure(text='Skip', command=skip)
+    if isinstance(current_timer, Countup):
+        skip_button.configure(text='Stop', command=stop)
+    pause_button.configure(text='Pause', command=pause)
+    if paused:
+        pause_button.configure(text='Resume', command=resume)
     try:
         root.update()
         root.update_idletasks()
@@ -182,7 +280,7 @@ while True:
             elif c[0] == 'countdown': # MUST SET UP
                 print('created countdown')
                 current_timer = Countdown(parse_hms(c[1]))
-                extras = cmd.split('-')[1:]
+                extras = cmd.split('$')[1:]
                 for arg in extras:
                     t = arg[:arg.find('(')]
                     s = arg[arg.find('(')+1:arg.find(')', arg.find(')')+1)]
@@ -197,7 +295,7 @@ while True:
 
             elif c[0] == 'play':
                 print('played sound')
-                f = cmd[cmd.find('"'):cmd.find('"', cmd.find('"')+1)]
+                f = audiofolder+cmd[cmd.find('"'):cmd.find('"', cmd.find('"')+1)]
                 Thread(target=audio.play_wav, args=(f,)).start()
 
             i += 1
@@ -208,8 +306,8 @@ while True:
                 continue
             elif result is True:
                 rv = current_timer.returnval
-                if rv is not 1:
-                    log.write(timestamp()+' - '+prompt+' - %d' % rv)
+                #if rv is not 1:
+                #    log.write('\n'+timestamp()+' - '+prompt+' - %d' % rv)
                 current_timer = DeadTimer()
             else:
                 current_timer.returnval = False
@@ -220,7 +318,7 @@ while True:
                 
                 elif c[0] == 'play':
                     print('$ played sound')
-                    f = c[1][c[1].find('"'):c[1].find('"', c[1].find('"')+1)]
+                    f = audiofolder+c[1][c[1].find('"'):c[1].find('"', c[1].find('"')+1)]
                     Thread(target=audio.play_wav, args=(f,)).start()
 
     except (KeyboardInterrupt, SystemExit, TclError):
@@ -229,4 +327,5 @@ while True:
 
 # root.destroy()
 print('Process complete.')
+log.close()
 exit(0)
