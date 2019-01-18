5dbfc552b688bfba541373955ed963ba5947d668
ShadowElf37 <ykey-cohen@emeryweiner.org>
2019-01-18 06:24:56 -0600

    hotfix

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ import os
 from cx_Freeze import setup, Executable
 
 # Dependencies are automatically detected, but it might need fine tuning.
-build_exe_options = {"packages": ["tkinter", "pyaudio"], "include_files": ["tcl86t.dll", "tk86t.dll", "default_config.cfg", "default_log.log"]}#, "excludes": ["tkinter"]}
-os.environ['TCL_LIBRARY'] = r'C:\Program Files\Python35-32\tcl\tcl8.6'
-os.environ['TK_LIBRARY'] = r'C:\Program Files\Python35-32\tcl\tk8.6'
+build_exe_options = {"packages": ["tkinter", "pyaudio"], "include_files": ["ShowFiles/", "tcl86t.dll", "tk86t.dll"]}
+os.environ['TCL_LIBRARY'] = r'C:\Users\Key Cohen Office\AppData\Local\Programs\Python\Python35\tcl\tcl8.6'
+os.environ['TK_LIBRARY'] = r'C:\Users\Key Cohen Office\AppData\Local\Programs\Python\Python35\tcl\tk8.6'
 # Note that these paths are wrong :)
 
 # GUI applications require a different base on Windows (the default is for a
@@ -14,8 +14,9 @@ base = None
 if sys.platform == "win32":
     base = "Win32GUI"
 
-setup(  name = "Mr. Fair's Special Timer",
-        version = "1.0",
-        description = "Yovel made this, not you.",
-        options = {"build_exe": build_exe_options},
-        executables = [Executable("main.py", base=base)])
+setup(
+    name="Mr. Fair's Special Timer",
+    version="1.1",
+    description="Yovel made this, not you.",
+    options={"build_exe": build_exe_options},
+    executables=[Executable("main.py", base=base)])
