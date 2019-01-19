ea5e66cb2f628204ff6ccb3d29c3c4f3b70bbc7c
ShadowElf37 <ykey-cohen@emeryweiner.org>
2019-01-18 19:08:10 -0600

    hotfix

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 from cx_Freeze import setup, Executable
 
 # Dependencies are automatically detected, but it might need fine tuning.
-build_exe_options = {"packages": ["tkinter", "pyaudio"], "include_files": ["ShowFiles/", "tcl86t.dll", "tk86t.dll"]}
+build_exe_options = {"packages": ["tkinter", "pyaudio"], "include_files": ["ShowFiles/", "tcl86t.dll", "tk86t.dll", "favicon.ico"]}
 os.environ['TCL_LIBRARY'] = r'C:\Users\Key Cohen Office\AppData\Local\Programs\Python\Python35\tcl\tcl8.6'
 os.environ['TK_LIBRARY'] = r'C:\Users\Key Cohen Office\AppData\Local\Programs\Python\Python35\tcl\tk8.6'
 # Note that these paths are wrong :)
